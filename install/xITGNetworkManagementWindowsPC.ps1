42ba61dbed50b2035d72f7e2ffac1685c3db59a6
Sergey S. Betke <sergey.s.betke@yandex.ru>
2019-05-09 11:39:57 +0300

    style: Format DSC script

--- install/xITGNetworkManagementWindowsPC.ps1
+++ install/xITGNetworkManagementWindowsPC.ps1
@@ -7,28 +7,33 @@ configuration ITGNetworkManagementWindowsPC
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName cChoco
 
-    cChocoInstaller choco {
+    cChocoInstaller choco
+    {
         InstallDir = "${env:SystemDrive}\choco"
     }
 
-    Environment chocolatelyInstall {
-        Name      = 'chocolatelyInstall'
-        value     = "${env:SystemDrive}\choco\bin"
+    Environment chocolatelyInstall
+    {
+        Name = 'chocolatelyInstall'
+        value = "${env:SystemDrive}\choco\bin"
         DependsOn = @('[cChocoInstaller]choco')
     }
 
-    cChocoPackageInstaller VSCode {
-        Name      = 'vscode'
+    cChocoPackageInstaller VSCode
+    {
+        Name = 'vscode'
         DependsOn = @('[cChocoInstaller]choco')
     }
 
-    cChocoPackageInstaller git {
-        Name      = 'git.install'
+    cChocoPackageInstaller git
+    {
+        Name = 'git.install'
         DependsOn = @('[cChocoInstaller]choco')
     }
 
-    cChocoPackageInstaller NodeJS {
-        Name      = 'nodejs'
+    cChocoPackageInstaller NodeJS
+    {
+        Name = 'nodejs'
         DependsOn = @('[cChocoInstaller]choco')
     }
 
