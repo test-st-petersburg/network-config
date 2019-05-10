e58add4c4e436e8f8f7aa6806f81da8364e0d507
Sergey S. Betke <sergey.s.betke@yandex.ru>
2019-05-10 16:12:30 +0300

    refactor(ci): Добавить возможность указания компьютера

--- install/Modules/xITGDSCEnvironment/xITGDSCEnvironment.schema.psm1
+++ install/Modules/xITGDSCEnvironment/xITGDSCEnvironment.schema.psm1
@@ -1,48 +1,59 @@
 configuration ITGDSCEnvironment
 {
+    param
+    (
+        [string[]]$ComputerName = 'localhost'
+    )
+
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName PackageManagement -ModuleVersion 1.3.1
     Import-DscResource -ModuleName xPowerShellExecutionPolicy
     Import-DscResource -ModuleName xWinRM
 
-<#
-	PackageManagementSource PSGallery {
-        Ensure             = "Present"
-        Name               = "PSGallery"
-        ProviderName       = "PowerShellGet"
-        SourceLocation     = "https://www.powershellgallery.com/api/v2/"
-        InstallationPolicy = "Trusted"
-    }
-#>
+    Node $ComputerName
+    {
+        <#
+		PackageManagementSource PSGallery {
+			Ensure             = "Present"
+			Name               = "PSGallery"
+			ProviderName       = "PowerShellGet"
+			SourceLocation     = "https://www.powershellgallery.com/api/v2/"
+			InstallationPolicy = "Trusted"
+		}
+		#>
 
-    PackageManagement cChoco {
-        Ensure    = "Present"
-        Name      = "cChoco"
-        Source    = "PSGallery"
-    #    DependsOn = "[PackageManagementSource]PSGallery"
-    }
+        PackageManagement cChoco
+        {
+            Ensure = "Present"
+            Name = "cChoco"
+            Source = "PSGallery"
+            #    DependsOn = "[PackageManagementSource]PSGallery"
+        }
 
-    Service WinRMService {
-        Name        = "WinRM"
-        Ensure      = "Present"
-        StartupType = "Automatic"
-        State       = "Running"
-    }
+        Service WinRMService
+        {
+            Name = "WinRM"
+            Ensure = "Present"
+            StartupType = "Automatic"
+            State = "Running"
+        }
 
-<#
-	xWinRM WinRM {
-        Ensure                   = "Present"
-        Protocol                 = "HTTP"
-        HTTPPort                 = "5985"
-        Client_Basic             = "false"
-        Client_Digest            = "false"
-        Client_Kerberos          = "true"
-        Client_Negotiate         = "true"
-        Service_AllowUnencrypted = "false"
-	}
-#>
+        <#
+		xWinRM WinRM {
+			Ensure                   = "Present"
+			Protocol                 = "HTTP"
+			HTTPPort                 = "5985"
+			Client_Basic             = "false"
+			Client_Digest            = "false"
+			Client_Kerberos          = "true"
+			Client_Negotiate         = "true"
+			Service_AllowUnencrypted = "false"
+		}
+		#>
 
-    xPowerShellExecutionPolicy PowerShellExecutionPolicy {
-        ExecutionPolicy = "Unrestricted"
+        xPowerShellExecutionPolicy PowerShellExecutionPolicy
+        {
+            ExecutionPolicy = "Unrestricted"
+        }
     }
 }
--- install/Modules/xITGPSEnvironment/xITGPSEnvironment.schema.psm1
+++ install/Modules/xITGPSEnvironment/xITGPSEnvironment.schema.psm1
@@ -1,58 +1,67 @@
 ﻿configuration ITGPSEnvironment
 {
+    param
+    (
+        [string[]]$ComputerName = 'localhost'
+    )
+
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName PackageManagement -ModuleVersion 1.3.1
     Import-DscResource -ModuleName PowerShellModule -Name PSModuleResource
 
-    <#
-	PackageManagementSource PSGallery
+    Node $ComputerName
     {
-        Ensure = 'Present'
-        Name = 'PSGallery'
-        ProviderName = 'PowerShellGet'
-        SourceLocation = 'https://www.powershellgallery.com/api/v2/'
-        InstallationPolicy = 'Trusted'
-	}
-	#>
 
-    PSModuleResource xPowerShellExecutionPolicy
-    {
-        Module_Name = 'xPowerShellExecutionPolicy'
+        <#
+		PackageManagementSource PSGallery
+		{
+			Ensure = 'Present'
+			Name = 'PSGallery'
+			ProviderName = 'PowerShellGet'
+			SourceLocation = 'https://www.powershellgallery.com/api/v2/'
+			InstallationPolicy = 'Trusted'
+		}
+		#>
+
+        PSModuleResource xPowerShellExecutionPolicy
+        {
+            Module_Name = 'xPowerShellExecutionPolicy'
+        }
+
+        PSModuleResource cChoco
+        {
+            Module_Name = 'cChoco'
+        }
+
+        PSModuleResource xWinRM
+        {
+            Module_Name = 'xWinRM'
+        }
+
+        PSModuleResource xComputerManagement
+        {
+            Module_Name = 'xComputerManagement'
+        }
+
+        PSModuleResource xHyper-V
+        {
+            Module_Name = 'xHyper-V'
+        }
+
+        PSModuleResource Lability
+        {
+            Module_Name = 'Lability'
+        }
+
+        PSModuleResource xPendingReboot
+        {
+            Module_Name = 'xPendingReboot'
+        }
+
+        PSModuleResource xDownloadFile
+        {
+            Module_Name = 'xDownloadFile'
+        }
+
     }
-
-    PSModuleResource cChoco
-    {
-        Module_Name = 'cChoco'
-    }
-
-    PSModuleResource xWinRM
-    {
-        Module_Name = 'xWinRM'
-    }
-
-    PSModuleResource xComputerManagement
-    {
-        Module_Name = 'xComputerManagement'
-    }
-
-    PSModuleResource xHyper-V
-    {
-        Module_Name = 'xHyper-V'
-    }
-
-    PSModuleResource Lability
-    {
-        Module_Name = 'Lability'
-    }
-
-    PSModuleResource xPendingReboot
-    {
-        Module_Name = 'xPendingReboot'
-    }
-
-    PSModuleResource xDownloadFile
-    {
-        Module_Name = 'xDownloadFile'
-    }
-
 }
