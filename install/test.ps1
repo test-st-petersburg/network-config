b8d1e8949654df87235ed76a2859412b9ef14dcd
Sergey S. Betke <sergey.s.betke@yandex.ru>
2019-08-17 13:28:53 +0300

    style: Restyle test.ps1

--- install/test.ps1
+++ install/test.ps1
@@ -6,52 +6,59 @@ $VerbosePreference = [System.Management.Automation.ActionPreference]::Continue;
 
 Function Wait-Expect
 {
-<#
+    <#
 .SYNOPSIS
-	Keep receiving output from a background job until it matches a pattern.
-	The output will be appended to the log file as it's received.
-	When a match is found, the line with it will be returned as the result.
+    Keep receiving output from a background job until it matches a pattern.
+    The output will be appended to the log file as it's received.
+    When a match is found, the line with it will be returned as the result.
 
-	The wait may be limited by a timeout. If the match is not received within
-	the timeout, throws an error (unless the option -Quiet is used, then
-	just returns).
+    The wait may be limited by a timeout. If the match is not received within
+    the timeout, throws an error (unless the option -Quiet is used, then
+    just returns).
 
-	If the job completes without matching the pattern, the reaction is the same
-	as on the timeout.
+    If the job completes without matching the pattern, the reaction is the same
+    as on the timeout.
 #>
     [CmdletBinding()]
     param(
-		[parameter( Mandatory = $true )]
-		[System.IO.TextReader] $StreamReader,
         [parameter( Mandatory = $true )]
-		[string] $Pattern
-	)
+        [System.IO.TextReader] $StreamReader,
+        [parameter( Mandatory = $true )]
+        [string] $Pattern
+    )
 
-	$local:ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+    $local:ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 
-    do {
-		$StreamData = $StreamReader.Read();
-        while ( $StreamData -eq -1 ) {
+    do
+    {
+        $StreamData = $StreamReader.Read();
+        while ( $StreamData -eq -1 )
+        {
             Start-Sleep -Milliseconds 50;
             $StreamData = $StreamReader.Read();
-		};
-		switch ( [char]$StreamData ) {
-			"`r" {
-				Write-Verbose $script:VerboseBuffer;
-				$script:VerboseBuffer = "";
-			}
-			"`n" {}
-			Default {
-				$script:VerboseBuffer += [char] $StreamData;
-			}
-		}
-		$script:Buffer += [char] $StreamData;
+        };
+        switch ( [char]$StreamData )
+        {
+            "`r"
+            {
+                Write-Verbose $script:VerboseBuffer;
+                $script:VerboseBuffer = "";
+            }
+            "`n"
+            {
+            }
+            Default
+            {
+                $script:VerboseBuffer += [char] $StreamData;
+            }
+        }
+        $script:Buffer += [char] $StreamData;
         $SearchResults = ( $script:Buffer | Select-String $Pattern ).Matches;
     } while ( -not $SearchResults.Success );
 
-	Write-Verbose $script:VerboseBuffer;
-	$script:VerboseBuffer = "";
-	$script:Buffer = $script:Buffer.Remove( 0, $SearchResults.Index + $SearchResults.Length );
+    Write-Verbose $script:VerboseBuffer;
+    $script:VerboseBuffer = "";
+    $script:Buffer = $script:Buffer.Remove( 0, $SearchResults.Index + $SearchResults.Length );
 
 }
 
@@ -74,22 +81,21 @@ $pipeWriter.AutoFlush = $true;
 
 Wait-Expect -StreamReader $pipeReader -Pattern 'Login:' -Verbose;
 $pipeWriter.WriteLine( 'admin' );
-
 Wait-Expect -StreamReader $pipeReader -Pattern 'Password:' -Verbose;
-$pipeWriter.WriteLine( '' )
+$pipeWriter.WriteLine( '' );
 
 Wait-Expect -StreamReader $pipeReader -Pattern '] >' -Verbose;
-$pipeWriter.WriteLine( "/system identity print" )
-$pipeReader.ReadLine() | Write-Host
+$pipeWriter.WriteLine( "/system identity print" );
+$pipeReader.ReadLine() | Write-Host;
 
 Wait-Expect -StreamReader $pipeReader -Pattern '] >' -Verbose;
-$pipeWriter.WriteLine( "/system identity set name=${RouterOSVM}" )
+$pipeWriter.WriteLine( "/system identity set name=${RouterOSVM}" );
 
 Wait-Expect -StreamReader $pipeReader -Pattern '] >' -Verbose;
-$pipeWriter.WriteLine( "/system identity print" )
-$pipeReader.ReadLine() | Write-Host
-$pipeReader.ReadLine() | Write-Host
-$pipeReader.ReadLine() | Write-Host
+$pipeWriter.WriteLine( "/system identity print" );
+$pipeReader.ReadLine() | Write-Host;
+$pipeReader.ReadLine() | Write-Host;
+$pipeReader.ReadLine() | Write-Host;
 
 $npipe.Dispose();
-Stop-VM -Name $RouterOSVM -Verbose
+Stop-VM -Name $RouterOSVM -Verbose;
